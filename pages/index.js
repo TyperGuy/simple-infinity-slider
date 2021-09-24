e7e3d2e71cf83ba3e25cd1caea36a86836879e3c
DevSIAC <ray_tony@iMac-de-DevSIAC.local>
2021-09-24 12:15:10 +0100

    Abordafem refeita

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,27 @@
 import React from 'react';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Img from '../public/assets/parceiros/agt.png'
+
 function Home() {
   return (
     
     <div className={styles.slider}>
       <div className={styles.slide_track}>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/agt.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/bai.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/banco_sol.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/bir.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/bpc.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/confianca.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/dtser.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/ensa.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/giant.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/imprensa.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/minjusdh.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/nossa_seguros.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/seguranca_social.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/sme.png')}/>
-          </div>
-          <div className={styles.slide}>
-            <Image src={require('../public/assets/parceiros/unitel.png')}/>
-          </div>
+            <Image className={styles.img} src={require('../public/assets/parceiros/agt.png')}/>
+            <Image className={styles.img} src={require('../public/assets/parceiros/bai.png')}/>
+             <Image className={styles.img} src={require('../public/assets/parceiros/banco_sol.png')}/>     
+            <Image className={styles.img} src={require('../public/assets/parceiros/bir.png')}/>     
+            <Image className={styles.img} src={require('../public/assets/parceiros/bpc.png')}/>    
+            <Image className={styles.img} src={require('../public/assets/parceiros/confianca.png')}/>      
+            <Image className={styles.img} src={require('../public/assets/parceiros/dtser.png')}/>     
+            <Image className={styles.img} src={require('../public/assets/parceiros/ensa.png')}/>      
+            <Image className={styles.img} src={require('../public/assets/parceiros/giant.png')}/>     
+            <Image className={styles.img} src={require('../public/assets/parceiros/imprensa.png')}/>      
+            <Image className={styles.img} src={require('../public/assets/parceiros/minjusdh.png')}/>    
+            <Image className={styles.img} src={require('../public/assets/parceiros/nossa_seguros.png')}/>      
+            <Image className={styles.img} src={require('../public/assets/parceiros/seguranca_social.png')}/>     
+            <Image className={styles.img} src={require('../public/assets/parceiros/sme.png')}/>     
+            <Image className={styles.img} src={require('../public/assets/parceiros/unitel.png')}/>
       </div>
     </div>
   )
