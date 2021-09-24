a827b7b36687b57a599756acd37b3a97731a643e
DevSIAC <ray_tony@iMac-de-DevSIAC.local>
2021-09-24 10:57:06 +0100

    Corrigindo Timing do Slide

--- pages/index.js
+++ pages/index.js
@@ -52,6 +52,52 @@ function Home() {
           <div className={styles.slide}>
             <Image src={require('../public/assets/parceiros/unitel.png')}/>
           </div>
+
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/agt.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/bai.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/banco_sol.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/bir.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/bpc.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/confianca.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/dtser.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/ensa.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/giant.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/imprensa.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/minjusdh.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/nossa_seguros.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/seguranca_social.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/sme.png')}/>
+          </div>
+          <div className={styles.slide}>
+            <Image src={require('../public/assets/parceiros/unitel.png')}/>
+          </div>
       </div>
     </div>
   )
